e4d03d70e999e3fd2c97ccfa66c20d558d69994c
Paulo <paulocer@gmail.com>
2019-09-01 13:42:05 -0300

    Salva mainActivity para preservar dados ao girar a tela

--- app/src/main/java/matc89/exercicio2/MainActivity.java
+++ app/src/main/java/matc89/exercicio2/MainActivity.java
@@ -1,14 +1,47 @@
 package matc89.exercicio2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    
+    private TextView textView;
+    private Button btnTrocar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView)findViewById(R.id.textView);
+        btnTrocar = (Button)findViewById(R.id.btnTrocar);
+        btnTrocar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(this, OutraActivity.class);
+                startActivity(intent);
+                
+                String nome = "Clicou Botão";
+                CharSequence texto = String.format("Oi, %s!", nome);
+                textView.setText(texto);
+            }
+        });
     }
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        String nomeDigitado = savedInstanceState.getString("nomeDigitado");
+        textView.setText(nomeDigitado);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("nomeDigitado", textView.getText().toString());
+    }
+
+
 }
